f3d6c2113c89aec6c157c2f97666bce14239694f
Bilgehan Sandıkcı <59236563+Bilgehansandikci@users.noreply.github.com>
2023-12-15 18:55:45 +0300

    Sale items and history screen improved

--- Components/SaleItem.js
+++ Components/SaleItem.js
@@ -2,8 +2,9 @@
 import React, { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { Card, Title, Paragraph, IconButton } from 'react-native-paper';
+import StarRating from 'react-native-star-rating';
 
-const SaleItem = ({ title, price, dateTime, rating }) => {
+const SaleItem = ({ title, price, dateTime, rating, customerName, comment }) => {
   const [expanded, setExpanded] = useState(false);
 
   const handleToggleMode = () => {
@@ -13,7 +14,7 @@ const SaleItem = ({ title, price, dateTime, rating }) => {
   return (
     <Card style={styles.offerContainer}>
       <Card.Content style={styles.contentContainer}>
-        {/* Title and Edit Icon */}
+        {/* Title and Expand-Shrink Icon */}
         <View style={styles.titleContainer}>
           <Title style={styles.title}>{title}</Title>
           <IconButton
@@ -23,13 +24,33 @@ const SaleItem = ({ title, price, dateTime, rating }) => {
           />
         </View>
 
-        {/* Description */}
-        <Paragraph style={styles.description}>{price}</Paragraph>
+        {/* Date and Time */}
+        <Paragraph style={styles.saleDateTime}>{dateTime}</Paragraph>
+
+        {/* Price and Star Rating */}
+        <View style={styles.descriptionContainer}>
+          <StarRating
+            disabled={true}
+            maxStars={5}
+            rating={rating}
+            starSize={14}
+            fullStarColor="green"
+            emptyStarColor="grey"
+          />
+          <Paragraph style={styles.price}>{price}</Paragraph>
+        </View>
 
         {expanded && (
           <>
-            <Paragraph style={styles.saleDateTime}>{dateTime}</Paragraph>
-            <Paragraph style={styles.saleRating}>Rating: {rating} stars</Paragraph>
+            {customerName && (
+              <Paragraph style={styles.saleRating}>Customer: {customerName}</Paragraph>
+            )}
+            {comment && <View style={styles.reviewContainer}>
+              <Paragraph>{comment}</Paragraph>
+            </View>}
+            {customerName && !comment && (
+              <Paragraph style={styles.noCommentText}>No comments yet</Paragraph>
+            )}
           </>
         )}
       </Card.Content>
@@ -44,6 +65,14 @@ const styles = StyleSheet.create({
     borderWidth: 2,
     margin: 16,
   },
+  reviewContainer: {
+    backgroundColor: '#fff',
+    borderColor: '#f26f55',
+    borderWidth: 2,
+    margin: 16,
+    padding: 8,
+    borderRadius: 10,
+  },
   contentContainer: {
     padding: 16,
   },
@@ -56,17 +85,216 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
   },
-  description: {
-    marginBottom: 8,
-  },
   saleDateTime: {
     fontSize: 14,
     color: 'grey',
   },
+  descriptionContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 8,
+  },
+  price: {
+    fontSize: 16,
+    color: "Green",
+  },
   saleRating: {
     fontSize: 14,
-    color: 'green',
+    color: 'grey',
+  },
+  noCommentText: {
+    fontSize: 14,
+    fontStyle: 'italic',
+    color: 'grey',
   },
 });
 
 export default SaleItem;
+
+// // SaleItem.js
+// import React, { useState } from 'react';
+// import { StyleSheet, View } from 'react-native';
+// import { Card, Title, Paragraph, IconButton } from 'react-native-paper';
+// import StarRating from 'react-native-star-rating'; // Import a library for star rating
+
+// const SaleItem = ({ title, price, dateTime, rating, customerName, comment }) => {
+//   const [expanded, setExpanded] = useState(false);
+
+//   const handleToggleMode = () => {
+//     setExpanded(!expanded);
+//   };
+
+//   return (
+//     <Card style={styles.offerContainer}>
+//       <Card.Content style={styles.contentContainer}>
+//         {/* Title and Edit Icon */}
+//         <View style={styles.titleContainer}>
+//           <Title style={styles.title}>{title}</Title>
+//           <IconButton
+//             icon={expanded ? 'chevron-up' : 'chevron-down'}
+//             size={20}
+//             onPress={handleToggleMode}
+//           />
+//         </View>
+
+//         {/* Description */}
+//         <View style={styles.descriptionContainer}>
+//           <Paragraph style={styles.price}>{price}</Paragraph>
+//           <View style={styles.ratingContainer}>
+//             <StarRating
+//               disabled={true}
+//               maxStars={5}
+//               rating={rating}
+//               starSize={14}
+//               fullStarColor="green"
+//               emptyStarColor="grey"
+//             />
+//             <Paragraph style={styles.ratingText}>{`${rating}/5`}</Paragraph>
+//           </View>
+//         </View>
+
+//         {expanded && (
+//           <>
+//             <Paragraph style={styles.saleDateTime}>{dateTime}</Paragraph>
+//             {comment ? (
+//               <>
+//                 <Paragraph style={styles.saleRating}>Customer: {customerName}</Paragraph>
+//                 <Paragraph>{comment}</Paragraph>
+//               </>
+//             ) : (
+//               <Paragraph style={styles.noCommentText}>No comments yet</Paragraph>
+//             )}
+//           </>
+//         )}
+//       </Card.Content>
+//     </Card>
+//   );
+// };
+
+// const styles = StyleSheet.create({
+//   offerContainer: {
+//     backgroundColor: '#fff',
+//     borderColor: '#f26f55',
+//     borderWidth: 2,
+//     margin: 16,
+//   },
+//   contentContainer: {
+//     padding: 16,
+//   },
+//   titleContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     justifyContent: 'space-between',
+//   },
+//   title: {
+//     fontSize: 18,
+//     fontWeight: 'bold',
+//   },
+//   descriptionContainer: {
+//     flexDirection: 'row',
+//     justifyContent: 'space-between',
+//     alignItems: 'center',
+//     marginTop: 8,
+//   },
+//   price: {
+//     fontSize: 16,
+//   },
+//   ratingContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//   },
+//   ratingText: {
+//     marginLeft: 4,
+//     fontSize: 14,
+//     color: 'grey',
+//   },
+//   saleDateTime: {
+//     fontSize: 14,
+//     color: 'grey',
+//   },
+//   saleRating: {
+//     fontSize: 14,
+//     color: 'grey',
+//   },
+//   noCommentText: {
+//     fontSize: 14,
+//     fontStyle: 'italic',
+//     color: 'grey',
+//   },
+// });
+
+// export default SaleItem;
+
+// // // SaleItem.js
+// // import React, { useState } from 'react';
+// // import { StyleSheet, View } from 'react-native';
+// // import { Card, Title, Paragraph, IconButton } from 'react-native-paper';
+
+// // const SaleItem = ({ title, price, dateTime, rating }) => {
+// //   const [expanded, setExpanded] = useState(false);
+
+// //   const handleToggleMode = () => {
+// //     setExpanded(!expanded);
+// //   };
+
+// //   return (
+// //     <Card style={styles.offerContainer}>
+// //       <Card.Content style={styles.contentContainer}>
+// //         {/* Title and Edit Icon */}
+// //         <View style={styles.titleContainer}>
+// //           <Title style={styles.title}>{title}</Title>
+// //           <IconButton
+// //             icon={expanded ? 'chevron-up' : 'chevron-down'}
+// //             size={20}
+// //             onPress={handleToggleMode}
+// //           />
+// //         </View>
+
+// //         {/* Description */}
+// //         <Paragraph style={styles.description}>{price}</Paragraph>
+
+// //         {expanded && (
+// //           <>
+// //             <Paragraph style={styles.saleDateTime}>{dateTime}</Paragraph>
+// //             <Paragraph style={styles.saleRating}>Rating: {rating} stars</Paragraph>
+// //           </>
+// //         )}
+// //       </Card.Content>
+// //     </Card>
+// //   );
+// // };
+
+// // const styles = StyleSheet.create({
+// //   offerContainer: {
+// //     backgroundColor: '#fff',
+// //     borderColor: '#f26f55',
+// //     borderWidth: 2,
+// //     margin: 16,
+// //   },
+// //   contentContainer: {
+// //     padding: 16,
+// //   },
+// //   titleContainer: {
+// //     flexDirection: 'row',
+// //     alignItems: 'center',
+// //     justifyContent: 'space-between',
+// //   },
+// //   title: {
+// //     fontSize: 18,
+// //     fontWeight: 'bold',
+// //   },
+// //   description: {
+// //     marginBottom: 8,
+// //   },
+// //   saleDateTime: {
+// //     fontSize: 14,
+// //     color: 'grey',
+// //   },
+// //   saleRating: {
+// //     fontSize: 14,
+// //     color: 'green',
+// //   },
+// // });
+
+// // export default SaleItem;
--- Screens/HistoryScreen.js
+++ Screens/HistoryScreen.js
@@ -7,10 +7,21 @@ const HistoryScreen = () => {
   const [sales, setSales] = useState([
     {
       id: '1',
-      title: 'Product 1',
+      title: 'Pastry Box',
       price: '$19.99',
       dateTime: '2023-12-01 08:30 AM',
       rating: 4,
+      customerName: "John Doe",
+      comment:null,
+    },
+    {
+      id: '2',
+      title: 'Product 2',
+      price: '$29.99',
+      dateTime: '2023-12-02 10:45 AM',
+      rating: 2,
+      customerName: "John Dove",
+      comment:null,
     },
     {
       id: '2',
@@ -18,6 +29,8 @@ const HistoryScreen = () => {
       price: '$29.99',
       dateTime: '2023-12-02 10:45 AM',
       rating: 5,
+      customerName: "Joe Boe",
+      comment:"It was just as fresh! would recommend."
     },
     // Add more sale items as needed
   ]);
@@ -31,6 +44,8 @@ const HistoryScreen = () => {
             price={item.price}
             dateTime={item.dateTime}
             rating={item.rating}
+            customerName={item.customerName}
+            comment={item.comment}
           />
       ))}
     </ScrollView>
